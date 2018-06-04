2df64febd8698d950891a5ceeb471a442cc31f89
EduardoSSR <eduardo_ssr@hotmail.com>
2018-06-04 14:43:05 +0000

    Descrição de conexão

--- humidity-temperature-sensor/humidity-temperature-sensor.py
+++ humidity-temperature-sensor/humidity-temperature-sensor.py
@@ -1,6 +1,13 @@
 # Programa : Sensor de temperatura DHT11 com Raspberry Pi B+
-# Autor : FILIPEFLOP
- 
+# Autor : Eduardo_ssr
+
+#
+#   O programa funciona no modo BCM, logo o pino de dados do DHT22 deve 
+#   ser conectado no GPIO.23, pino 16. Com um resistor de pull-up  entre 
+#   o pino de dados e o VCC(3V3).
+#
+# 
+#
 # Carrega as bibliotecas
 import Adafruit_DHT
 import RPi.GPIO as GPIO
@@ -10,7 +17,7 @@ import time
 #sensor = Adafruit_DHT.DHT11
 sensor = Adafruit_DHT.DHT22
  
-GPIO.setmode(GPIO.BOARD)
+#GPIO.setmode(GPIO.BOARD)
  
 # Define a GPIO conectada ao pino de dados do sensor
 pino_sensor = 25
@@ -23,8 +30,8 @@ while(1):
    umid, temp = Adafruit_DHT.read_retry(sensor, pino_sensor);
    # Caso leitura esteja ok, mostra os valores na tela
    if umid is not None and temp is not None:
-     print ("Temperatura = {0:0.1f}  Umidade = {1:0.1f}n").format(temp, umid);
-     print ("Aguarda 5 segundos para efetuar nova leitura...n");
+     print ("Temperatura = {0:0.1f}  Umidade = {1:0.1f}\n").format(temp, umid);
+     print ("Aguarda 5 segundos para efetuar nova leitura...\n");
      time.sleep(5)
    else:
      # Mensagem de erro de comunicacao com o sensor
