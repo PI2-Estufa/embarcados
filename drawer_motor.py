523974c650ae3422f28580764837266d7fdb22bc
Adailson <adailson2@gmail.com>
2018-06-22 00:58:39 -0200

    Add drawer motor

--- drawer_motor.py
+++ drawer_motor.py
@@ -32,6 +32,9 @@ GPIO.setup(pinReed1, GPIO.IN)
 GPIO.setup(pinReed2, GPIO.IN)
 GPIO.setup(pinPorta, GPIO.IN)
 
+GPIO.output(pinMotor1, GPIO.LOW)
+GPIO.output(pinMotor2, GPIO.LOW)
+
 estado = 'i' 
 
 motor1 = 0
@@ -41,8 +44,8 @@ motor2 = 0
 
 reedPorta = 1
 c = 0
-reed1 =0
-reed2 =1
+reed1 = 0
+reed2 = 1
 
 def start(porta,comando,reed1,reed2):
     
@@ -56,7 +59,7 @@ def start(porta,comando,reed1,reed2):
     motor2 = 0
     estado = 0
     
-def eFechado(porta, comando,reed1,reed2):
+def eFechado(porta,comando,reed1,reed2):
     
     global estado
     global motor1
@@ -65,27 +68,27 @@ def eFechado(porta, comando,reed1,reed2):
     motor1 = 0
     motor2 = 0
     
-    #print ("Gaveta fechada. ")
+    print ("Gaveta fechada. ")
     #sleep(t)
-    if ((porta == '1') and (comando == '0' or comando =='1')) or (comando == '0'):
+    if ((porta == 1) and (comando == '0' or comando =='1')) or (comando == '0'):
         estado = 0
-    elif (comando == '1' and porta == '0'):
+    elif (comando == '1' and porta == 0):
         estado = 1
 
 
-def eAbrindo(porta, comando, reed1, reed2):
+def eAbrindo(porta,comando, reed1, reed2):
     
     global estado
     global motor1 
     global motor2
-    #print("Abrindo gaveta")
-    if reed1 == 1: 
+    print("Abrindo gaveta")
+    if reed2 == 0: 
         motor1 = 0
         motor2 = 1
         estado = 1
     else:
         motor1 = 0
-        motor1 = 0
+        motor2 = 0
         estado = 2
 	#sleep(t)
     
@@ -96,7 +99,7 @@ def eAberto(porta, comando, reed1, reed2):
     global motor1
     global motor2
 
-    #print("Gaveta aberta")
+    print("Gaveta aberta")
     motor1 = 0 
     motor2 = 0
     if(comando == '1'):
@@ -109,8 +112,8 @@ def eFechando(porta, comando, reed1, reed2):
     global estado
     global motor1
     global motor2
-    #print ("Fechando Gaveta")  
-    if reed2 == 1: 	
+    print ("Fechando Gaveta")  
+    if reed1 == 0: 	
         motor1 = 1
         motor2 = 0
         estado = 3
@@ -123,7 +126,7 @@ def maquinaEstados(porta, comando, reed1, reed2 ):
     global estado 
     
     if estado == 'i':
-        start(porta,comando, reed1, reed2)
+        start(porta, comando, reed1, reed2)
     elif estado == 0:
         eFechado(porta, comando, reed1, reed2)
     elif estado == 1:
@@ -135,48 +138,63 @@ def maquinaEstados(porta, comando, reed1, reed2 ):
     else:
         start(porta, comando, reed1, reed2) 
 
+def estadoMotor(m1,m2):	
+	if m1 == 1:
+		GPIO.output(pinMotor1, GPIO.HIGH)
+	else:
+		GPIO.output(pinMotor1, GPIO.LOW)
+	
+	if m2 == 1:
+		GPIO.output(pinMotor2, GPIO.HIGH)
+	else:
+		GPIO.output(pinMotor2, GPIO.LOW)
+
 def controleGaveta():
     global c
     global reedPorta
     global reed1
     global reed2
+    global motor1
+    global motor2
    # interrupt = 0
     while(True):
         maquinaEstados(reedPorta, c, reed1, reed2)
-        sleep(0.01)
+        sleep(0.1)
+        estadoMotor(motor1,motor2)
        # if p == '1' and estado == 2:
        #     interrupt =1
 
 gaveta = threading.Thread(target=controleGaveta)  
 gaveta.start()
 
+        
+
 while(True):
 
         #reedPorta = input("Qual o estado da porta(0 = aberta e 1 = fechada): ")
 	if GPIO.input(pinPorta)	== GPIO.HIGH:
 		reedPorta = 1 #porta fechada
+		print("Porta fechada")
 	else:
 		reedPorta = 0 #porta aberta
+		print("Porta aberta")
         
 	if GPIO.input(pinReed1) == GPIO.HIGH:
 		reed1 = 1
+		print("Reed traseiro ativado")
 	else:
 		reed1 = 0
+		print("Reed traseiro desativado")
 	
-	if GPIO.input(pinReed1) == GPIO.HIGH:
+	if GPIO.input(pinReed2) == GPIO.HIGH:
 		reed2 = 1
+		print("Reed dianteiro ativado")
 	else:
-		reed2 = 0   
+		reed2 = 0
+		print("Reed dianteiro desativado")
+	c = input("Quer a gaveta aberta ou fechada(0 = fechada e 1 = aberta): ")	
 	
-	c = input("Quer a gaveta aberta ou fechada(0 = fechar e 1 = abrir): ")
-	
-	if motor1 == 1:
-		GPIO.output(pinMotor1, GPIO.HIGH)
-	else:
-		GPIO.output(pinMotor1, GPIO.LOW)
-	
-	if motor2 == 1:
-		GPIO.output(pinMotor2, GPIO.HIGH)
-	else:
-		GPIO.output(pinMotor2, GPIO.LOW)
+        
+
+
 		
